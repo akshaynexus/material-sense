41968ffd3551cc78354314b5a00a4ee3b9832ec9
Akshay CM <akshaycm@hotmail.com>
2020-04-04 14:41:04 +0300

    Fix key for coins cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -6,7 +6,7 @@ import CardCoin from "./cards/CardCoin";
 import Topbar from "./Topbar";
 import SectionHeader from "./typo/SectionHeader";
 import Loading from "./common/Loading";
-import config from '../config.js'
+import config from "../config.js";
 const styles = theme => ({
   grid: {
     width: 1100
@@ -15,51 +15,61 @@ const styles = theme => ({
 
 class Cards extends Component {
   state = {
-    loading:true,
-    pooldata:null,
-  }
-  async componentDidMount(){
+    loading: true,
+    pooldata: null
+  };
+  async componentDidMount() {
     const APIPath = config.poolapiurl;
     const response = await fetch(APIPath + "pools");
     const data = await response.json();
-    this.setState({pooldata: data.pools,loading:false});
+    this.setState({ pooldata: data.pools, loading: false });
   }
-   hashformat(value, decimal, unit) {
+  hashformat(value, decimal, unit) {
     if (decimal === 0) {
-        return value + unit;
+      return value + unit;
     } else {
-        var si = [
-            { value: 1, symbol: "" },
-            { value: 1e3, symbol: "k" },
-            { value: 1e6, symbol: "M" },
-            { value: 1e9, symbol: "G" },
-            { value: 1e12, symbol: "T" },
-            { value: 1e15, symbol: "P" },
-            { value: 1e18, symbol: "E" },
-            { value: 1e21, symbol: "Z" },
-            { value: 1e24, symbol: "Y" },
-        ];
-        for (var i = si.length - 1; i > 0; i--) {
-            if (value >= si[i].value) {
-                break;
-            }
+      var si = [
+        { value: 1, symbol: "" },
+        { value: 1e3, symbol: "k" },
+        { value: 1e6, symbol: "M" },
+        { value: 1e9, symbol: "G" },
+        { value: 1e12, symbol: "T" },
+        { value: 1e15, symbol: "P" },
+        { value: 1e18, symbol: "E" },
+        { value: 1e21, symbol: "Z" },
+        { value: 1e24, symbol: "Y" }
+      ];
+      for (var i = si.length - 1; i > 0; i--) {
+        if (value >= si[i].value) {
+          break;
         }
-        return (value / si[i].value).toFixed(decimal).replace(/\.0+$|(\.[0-9]*[1-9])0+$/, "$1") + ' ' + si[i].symbol + unit;
+      }
+      return (
+        (value / si[i].value)
+          .toFixed(decimal)
+          .replace(/\.0+$|(\.[0-9]*[1-9])0+$/, "$1") +
+        " " +
+        si[i].symbol +
+        unit
+      );
     }
-}
+  }
   buildCoinCards() {
-   return this.state.pooldata.map(coin => (<div key={1}>
-     <CardCoin
-    coin={coin.coin.name}
-    ticker={coin.coin.type}
-    algo={coin.coin.algorithm}
-    minercount={coin.poolStats.connectedMiners}
-    poolhashrate={this.hashformat(coin.poolStats.poolHashrate,2, 'H/s')}
-    diff={this.hashformat(coin.networkStats.networkDifficulty,2, '')}
-    fee={coin.poolFeePercent + "%"}
-    poolid={coin.id}
-    />
-  <br></br> </div>))
+    return this.state.pooldata.map((coin, index) => (
+      <div key={index}>
+        <CardCoin
+          coin={coin.coin.name}
+          ticker={coin.coin.type}
+          algo={coin.coin.algorithm}
+          minercount={coin.poolStats.connectedMiners}
+          poolhashrate={this.hashformat(coin.poolStats.poolHashrate, 2, "H/s")}
+          diff={this.hashformat(coin.networkStats.networkDifficulty, 2, "")}
+          fee={coin.poolFeePercent + "%"}
+          poolid={coin.id}
+        />
+        <br />{" "}
+      </div>
+    ));
   }
   render() {
     // const handleChange = (event) => {
@@ -87,8 +97,11 @@ class Cards extends Component {
                   subtitle="Available coins to mine"
                   val={selectedindex}
                 />
-                {(this.state.loading|| !this.state.pooldata) ?
-                <Loading  overlay={true} loading={this.state.loading} />:this.buildCoinCards()}
+                {this.state.loading || !this.state.pooldata ? (
+                  <Loading overlay={true} loading={this.state.loading} />
+                ) : (
+                  this.buildCoinCards()
+                )}
               </Grid>
             </Grid>
           </Grid>
