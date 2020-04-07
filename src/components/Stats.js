d2fe66f1f3d4ef702ee3f40aac15095ea08aae02
vinayak <vinayakdhadda2417@gmail.com>
2020-04-08 00:00:07 +0530

    Mobile responsive fixes

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -14,7 +14,8 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 import "./Stats.css";
-import { XAxis, YAxis, Tooltip, Legend, AreaChart, Area
+import {
+    XAxis, YAxis, Tooltip, Legend, AreaChart, Area
 } from 'recharts';
 
 const useStyles = makeStyles({
@@ -66,80 +67,85 @@ const Stats = (props) => {
 
 
     useEffect(() => {
+        const getGraphData = async () => {
+            const response = await fetch(
+                `https://mineit.io/api/pools/${poolid}/performance`
+            );
+
+            const data = await response.json();
+
+            data.stats.map((stats, index) => (
+                setPoolHashrates(poolHashrates => [...poolHashrates, { value: (stats.poolHashrate / 1000000000), name: stats.created.substring(11, 16) }]),
+                setConnectedMiners(connectedMiners => [...connectedMiners, { value: stats.connectedMiners, name: stats.created.substring(11, 16) }]),
+                setNetworkHashrates(networkHashRates => [...networkHashRates, { value: (stats.networkHashrate / 1000000000), name: stats.created.substring(11, 16) }]),
+                setNetworkDifficulty(networkDifficulty => [...networkDifficulty, { value: stats.networkDifficulty, name: stats.created.substring(11, 16) }])
+            ));
+        };
+
+        const getPoolData = async () => {
+            const response = await fetch(`https://mineit.io/api/pools/${poolid}`);
+            const data = await response.json();
+            setPoolData({
+                poolHashRate: data.pool.poolStats.poolHashrate,
+                miners: data.pool.poolStats.connectedMiners,
+                networkHashrate: data.pool.networkStats.networkHashrate,
+                networkDifficulty: data.pool.networkStats.networkDifficulty,
+                poolFee: data.pool.poolFeePercent,
+                paymentThreshold: data.pool.paymentProcessing.minimumPayment + " " + data.pool.coin.type,
+                paymentScheme: data.pool.paymentProcessing.payoutScheme,
+                blockchainHeight: data.pool.networkStats.blockHeight,
+                connectedPeers: data.pool.networkStats.connectedPeers
+            })
+        }
+
+
         getGraphData();
         getPoolData();
     }, [])
 
-    const getGraphData = async () => {
-        const response = await fetch(
-            `https://mineit.io/api/pools/${poolid}/performance`
-        );
 
-        const data = await response.json();
 
-        data.stats.map((stats, index) => (
-            setPoolHashrates(poolHashrates => [...poolHashrates, { value: (stats.poolHashrate / 1000000000), name: stats.created.substring(11, 16) }]),
-            setConnectedMiners(connectedMiners => [...connectedMiners, { value: stats.connectedMiners, name: stats.created.substring(11, 16) }]),
-            setNetworkHashrates(networkHashRates => [...networkHashRates, { value: (stats.networkHashrate / 1000000000), name: stats.created.substring(11, 16) }]),
-            setNetworkDifficulty(networkDifficulty => [...networkDifficulty, { value: stats.networkDifficulty, name: stats.created.substring(11, 16) }])
-        ));
-    };
-
-    const CardChart = (data,CardSubtitle,CardLateststat) =>{
+    const CardChart = (data, CardSubtitle, CardLateststat) => {
         return <Card className={classes.root}>
-                <CardContent>
-                    <AreaChart width={800} height={400} data={data}
+            <CardContent>
+                <AreaChart width={800} height={400} data={data}
                     margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
                     <defs>
-                         <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                        <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
                             <stop offset="5%" stopColor="#167ef5" stopOpacity={0.8} />
                             <stop offset="95%" stopColor="#167ef5" stopOpacity={0} />
                         </linearGradient>
                     </defs>
-                <XAxis dataKey="name" />
-                <YAxis />
-                <Tooltip />
-                <Area type="monotone" dataKey="value" stroke="#b5ceeb" fill="url(#colorPv)" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Area type="monotone" dataKey="value" stroke="#b5ceeb" fill="url(#colorPv)" />
                 </AreaChart>
-            <Typography variant="h5" component="h2">
-                {CardLateststat}
-            </Typography>
-            <Typography variant="body" component="h5">
-                {CardSubtitle}
-            </Typography>
-        </CardContent>
-    </Card>
+                <Typography variant="h5" component="h2">
+                    {CardLateststat}
+                </Typography>
+                <Typography variant="body" component="h5">
+                    {CardSubtitle}
+                </Typography>
+            </CardContent>
+        </Card>
     }
 
-    const CardInfo = (data,Title) => {
-    return  <Card className={classes.valueItems}>
-              <CardHeader
-                 avatar={
+    const CardInfo = (data, Title) => {
+        return <Card className={classes.valueItems}>
+            <CardHeader
+                avatar={
                     <Avatar aria-label={Title} className={classes.avatar}>
-                    {/*Put icon related to cardinfo here*/}
+                        {/*Put icon related to cardinfo here*/}
                     </Avatar>
                 }
                 title={Title}
                 subheader={data}
-                />
-            </Card>
+            />
+        </Card>
     }
 
-    const getPoolData = async () => {
-        const response = await fetch(`https://mineit.io/api/pools/${poolid}`);
-        const data = await response.json();
-        setPoolData({
-            poolHashRate: data.pool.poolStats.poolHashrate,
-            miners: data.pool.poolStats.connectedMiners,
-            networkHashrate: data.pool.networkStats.networkHashrate,
-            networkDifficulty: data.pool.networkStats.networkDifficulty,
-            poolFee: data.pool.poolFeePercent,
-            paymentThreshold: data.pool.paymentProcessing.minimumPayment + " " + data.pool.coin.type,
-            paymentScheme: data.pool.paymentProcessing.payoutScheme,
-            blockchainHeight: data.pool.networkStats.blockHeight,
-            connectedPeers: data.pool.networkStats.connectedPeers
-        })
-    }
+
 
     const classes = useStyles();
     return (
@@ -148,7 +154,7 @@ const Stats = (props) => {
             <Topbar currentPath={"/stats"} />
             <div className="container">
                 <Grid container spacing={2} direction="row">
-                    <Grid item xs={6}>
+                    <Grid item xs={12} sm={12} md={6}>
                         <Grid
                             container
                             direction="column"
@@ -156,11 +162,11 @@ const Stats = (props) => {
                             alignItems="center"
                             spacing={3}
                         >
-                        {CardChart(poolHashrates,"Pool Hashrate",poolData.poolHashRate)}
+                            {CardChart(poolHashrates, "Pool Hashrate", poolData.poolHashRate)}
                         </Grid>
                     </Grid>
 
-                    <Grid item xs={6}>
+                    <Grid item xs={12} sm={12} md={6}>
                         <Grid
                             container
                             direction="column"
@@ -168,7 +174,7 @@ const Stats = (props) => {
                             alignItems="center"
                             spacing={3}
                         >
-                            {CardChart(connectedMiners,"Miners",poolData.miners + " Miners")}
+                            {CardChart(connectedMiners, "Miners", poolData.miners + " Miners")}
                         </Grid>
                     </Grid>
 
@@ -178,8 +184,8 @@ const Stats = (props) => {
                             direction="row"
                             justify="space-evenly"
                             alignItems="center">
-                            {CardInfo(poolData.blockchainHeight,"Blockchain Height")}
-                            {CardInfo(poolData.connectedPeers,"Connected Peers")}
+                            {CardInfo(poolData.blockchainHeight, "Blockchain Height")}
+                            {CardInfo(poolData.connectedPeers, "Connected Peers")}
                             <Card className={classes.valueItems}>
                                 <CardContent>
                                     <Typography variant="h5" component="h2">
@@ -194,13 +200,13 @@ const Stats = (props) => {
                                 </CardContent>
                             </Card>
 
-                            {CardInfo(poolData.poolFee + "%","Pool Fee")}
+                            {CardInfo(poolData.poolFee + "%", "Pool Fee")}
 
                         </Grid>
 
                     </Grid>
 
-                    <Grid item xs={6}>
+                    <Grid item xs={12} sm={12} md={6}>
                         <Grid
                             container
                             direction="column"
@@ -208,11 +214,11 @@ const Stats = (props) => {
                             alignItems="center"
                             spacing={3}
                         >
-                            {CardChart(networkHashRates,"Network Hashrate",poolData.networkHashrate)}
+                            {CardChart(networkHashRates, "Network Hashrate", poolData.networkHashrate)}
                         </Grid>
                     </Grid>
 
-                    <Grid item xs={6}>
+                    <Grid item xs={12} sm={12} md={6}>
                         <Grid
                             container
                             direction="column"
@@ -220,7 +226,7 @@ const Stats = (props) => {
                             alignItems="center"
                             spacing={3}
                         >
-                            {CardChart(networkDifficulty,"Network Difficulty",poolData.networkDifficulty)}
+                            {CardChart(networkDifficulty, "Network Difficulty", poolData.networkDifficulty)}
                         </Grid>
                     </Grid>
                 </Grid>
